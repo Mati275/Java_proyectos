dde30ef07440d53513d1a33d9d631641930fbd8c
Matias <matitrepat@gmail.com>
2026-08-14 16:39:44 +0200

    .

--- Hundir_la_flota/src/domain/model/Player.java
+++ Hundir_la_flota/src/domain/model/Player.java
@@ -1,4 +1,30 @@
 package domain.model;
 
 public class Player {
+
+    // ATTRIBUTES
+    private Ship[] ships;
+    private String name;
+    private Cell[] board;
+
+    // CONSTRUCTOR
+    /**
+     * This contructor generates automatically the ships
+     */
+    public Player( String name ){
+
+        generateShips();
+
+    }
+    // GETTERS
+
+    // SETTERS
+
+
+    // **************
+    // OTHER METHODS
+    // **************
+
+
+
 }
